053c6860d30067722356a027cb7d5e64fd29b9d9
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-09-30 19:56:59 +0100

    fix task 3

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,3 +1,3 @@
-export default function getSumOfHoods(initialNumber = 89, expansion1989 = 19, expansion2019 = 0) {
+export default function getSumOfHoods(initialNumber = 0, expansion1989 = 19, expansion2019 = 89) {
   return initialNumber + expansion1989 + expansion2019;
 }
