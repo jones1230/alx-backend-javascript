5c5711fd4ce5f9a8d388249ca87cbd27c34e3e15
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-10-03 21:49:26 +0100

    array promises in task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignUp(firstName, lastName, fileName) {
-  return Promise.allSettled(signUpUser(firstName, lastName), uploadPhoto(fileName))
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((res) => (
       res.map((res) => ({
         status: res.status,
