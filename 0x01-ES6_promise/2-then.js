7e4a0034b49dcf43ca9d69d8743c6732b4fa1966
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-10-03 10:48:19 +0100

    correct output string

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,5 +3,5 @@
 export default function handleResponseFromAPI(promise) {
   promise.then(() => ({ status: 200, body: success }))
     .catch(() => new Error())
-    .finally(() => console.log('Got a reponse from the API'));
+    .finally(() => console.log('Got a response from the API'));
 }
