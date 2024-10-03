708e130b110d06b00d072d7ffd227fd2f1d31cd7
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-10-03 22:17:58 +0100

    solve eslint error

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,11 +1,11 @@
 export default function divideFunction(numerator, denominator) {
   try {
-    const res = numerator/denominator;
-    if (isFinite(res)) {
+    const res = numerator / denominator;
+    if (Number.isFinite(res)) {
       return res;
     }
     throw Error('cannot divide by 0');
-  } catch(error) {
+  } catch (error) {
     return error;
   }
 }
