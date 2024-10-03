1d4146707a73ef57b197afb88a74e7a88d19e405
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-10-03 21:47:35 +0100

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,9 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignUp(firstName, lastName, fileName) {
   return Promise.allSettled(signUpUser(firstName, lastName), uploadPhoto(fileName))
-    .then((res) => res);
+    .then((res) => (
+      res.map((res) => ({
+        status: res.status,
+        value: res.status === 'fulfilled' ? res.value : String(res.reason),
+      }))));
 }
