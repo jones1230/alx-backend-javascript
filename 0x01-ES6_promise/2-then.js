47c20866d88e98fa8c214e6bc4d8810bc18e7409
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-10-03 10:56:43 +0100

    facepalm, add return keyword

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({ status: 200, body: 'success' }), () => new Error())
+  return promise.then(() => ({ status: 200, body: 'success' }), () => new Error())
     .finally(() => console.log('Got a response from the API'));
 }
