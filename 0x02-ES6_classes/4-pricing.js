826f3c9349b60328a720b6b99f07ac1a55736387
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-10-04 17:09:25 +0000

    task 4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -4,8 +4,40 @@ import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  /**
+   * @returns {Currency}
+   */
+  get currency() {
+    return this._currency;
+  }
+
+  /**
+   * @param {Currency} value
+   */
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('currency must be a Currency');
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
@@ -16,47 +48,5 @@ export default class Pricing {
       throw new TypeError('conversionRate must be a number');
     }
     return amount * conversionRate;
-  }
-
-  /**
-   * @return {amount}
-   */
-  get amount() {
-    return this._amount;
-  }
-
-  /**
-   * @param {amount} value
-   */
-  set amount(amount) {
-    if (!(amount instanceof Currency)) {
-      throw new TypeError('currency must be a Currency');
-    }
-    this._amount = amount;
-  }
-
-  /**
-   * @return {currency}
-   */
-  get currency() {
-    return this.currency;
-  }
-
-  /**
-   * @param {currency} value
-   */
-  set currency(currency) {
-    if (typeof currency !== 'number') {
-      throw new TypeError('amount must be a number');
-    }
-    this._currency = currency;
-  }
-
-  /**
-    * Function combines and amount, currency name and currency code and returns
-    * @returns {string}
-    */
-  displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 }
