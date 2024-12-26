7d31a12c2e145e839a13087cd84111bdd1dbd182
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-12-26 14:58:16 +0000

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,12 +6,12 @@
  *
  * @example
  * // Output:
- * // Welcome to ALX, what is your name?
+ * // Welcome to Holberton School, what is your name?
  * // Alice
  * // Your name is: Alice
  * // This important software is now closing
  */
-process.stdout.write('Welcome to ALX, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
